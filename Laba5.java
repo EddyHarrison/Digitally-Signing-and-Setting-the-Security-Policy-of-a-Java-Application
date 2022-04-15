421490444097ca66835120cb4265c58691a89c92
Kalinevich <52758256+EddyHarrison@users.noreply.github.com>
2022-04-15 11:45:47 +0300

    Add files via upload

---
+++ Laba5.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class Laba5 extends JFrame implements ActionListener {
+
+    private JPanel panel;
+    private JButton button;
+
+    private final JTextArea textArea;
+
+    public Control() {
+        panel = new JPanel();
+        panel.setBounds(100, 100, 100, 100);
+        panel.setBackground(Color.gray);
+
+        button = new JButton("Click here to write");
+        button.addActionListener(this);
+        panel.add(button);
+
+        textArea = new JTextArea();
+        textArea.setBackground(Color.white);
+        panel.add(textArea);
+        add(panel);
+
+
+        setSize(400, 200);
+        setBackground(Color.BLACK);
+        setTitle("Laba5");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("file"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                textArea.setText(line);
+            }
+        } catch (FileNotFoundException ex) {
+            textArea.setText("Ошибка чтения файла");
+        } catch (SecurityException ex) {
+            textArea.setText("Нет разрешения к доступу к файлу");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Laba5();
+    }
+
+
+}
